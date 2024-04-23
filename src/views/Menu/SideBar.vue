65b58de3deeccc810462b2d42dd95becc04cc1bd
andygggg <andyhello31468@gmail.com>
2024-04-23 12:35:03 +0800

    fix:navbar and sidebar初稿

---
+++ src/views/Menu/SideBar.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="sidebar_container">sidebar</div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+.sidebar_container {
+  background-color: rgb(117, 104, 235);
+  height: 100%;
+  width: 80px;
+}
+</style>
