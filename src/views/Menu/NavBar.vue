57a8e69282045c8aed362fb63db2d9c06ad111ee
andygggg <andyhello31468@gmail.com>
2024-05-02 16:48:59 +0800

    fix: 製作目錄搜尋功能

--- src/views/Menu/NavBar.vue
+++ src/views/Menu/NavBar.vue
@@ -3,6 +3,7 @@
     <nav class="navbar navbar-light" style="background-color: #e6e3fd">
       <div class="container-fluid">
         <a class="navbar-brand">Navbar</a>
+        <MakeSelect class="long"></MakeSelect>
         <form class="d-flex">
           <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
           <button class="btn btn-outline-success" type="submit">Search</button>
@@ -11,3 +12,13 @@
     </nav>
   </div>
 </template>
+
+<script setup>
+import MakeSelect from '../MakeComponents/MakeSelect.vue'
+</script>
+
+<style scoped>
+.long {
+  width: 30%;
+}
+</style>
