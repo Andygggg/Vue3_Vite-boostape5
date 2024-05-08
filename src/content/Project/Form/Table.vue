560f2362ecf6f1b483d88c9e8ebcbde5f2078601
andygggg <andyhello31468@gmail.com>
2024-05-08 16:48:08 +0800

    fix: table 增加分頁功能及排序功能(完成)

--- src/content/Project/Form/Table.vue
+++ src/content/Project/Form/Table.vue
@@ -2,7 +2,7 @@
   <div class="table_container">
     <MakeTable :table_header="table" :table_data="table_data" table_scroll="60vh">
       <template #cell(one)="data">
-        <span>{{ data.index }}</span>
+        <span>{{ data.value }}</span>
       </template>
       <template #cell(two)="data">
         <span style="color: rgb(255, 0, 242)">{{ data.item.two }}</span>
@@ -16,15 +16,15 @@ import { reactive } from 'vue'
 import MakeTable from '../../../views/MakeComponents/MakeTable.vue'
 
 const table = reactive([
-  { key: 'one', label: 'Number', sort: false },
+  { key: 'one', label: 'Number', sort: true },
   { key: 'two', label: 'name', sort: false },
   { key: 'date', label: '日期', sort: true },
   { key: 'four', label: '@', sort: false },
 ])
 const table_data = reactive([
-  { one: '1', two: 'Mark', date: '2023/5/6', four: '	@mdo' },
+  { one: '1', two: 'Jacob', date: '2023/5/6', four: '@mdo' },
   { one: '2', two: 'Jacob', date: '2023/5/3', four: '@fat' },
-  { one: '3', two: 'Jacob', date: '2023/5/1', four: '@fat' },
+  { one: '3', two: 'Jacob', date: '2023/5/1', four: '@mdo' },
   { one: '4', two: 'Jacob', date: '2023/5/3', four: '@fat' },
   { one: '5', two: 'Jacob', date: '2023/5/3', four: '@fat' },
   { one: '6', two: 'Jacob', date: '2023/5/4', four: '@fat' },
